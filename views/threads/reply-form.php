3d49aeb60476b7fe833990e1f2b21bffa4b33aec
genleeshin <genl.lee.shin@gmail.com>
2017-02-18 19:08:27 +0000

    added csrf to post

--- views/threads/reply-form.php
+++ views/threads/reply-form.php
@@ -1,5 +1,5 @@
 <form action="/thread/reply" method="post" id="form-reply" class="<?=is_logged()?'':'disabled'?>">
-	
+	<?=csrf_token_field()?>
 	<input type="hidden" name="id" value="<?=$post->id?>">
 	<div class="form-group">
 		<textarea name="body" id="body" cols="50" rows="10"></textarea>
